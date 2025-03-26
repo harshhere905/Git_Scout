fd20ec136f28cbff6d6f102711ab64da38fd28b7
Harshhere905 <hv96829@gmail.com>
2025-03-27 02:33:13 +0530

    added production built

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -17,7 +17,7 @@ passport.use(
         {
             clientID: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: "https://mern-github-repo-6ztj.onrender.com/api/auth/github/callback",
+            callbackURL: "http://localhost:5000/api/auth/github/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
             const user = await User.findOne({username: profile.username});
